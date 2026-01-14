dbfff014d5e0a2f71432ea3abcfe5c9d433aa3c6
Stephen <wangyafei.stephen@bytedance.com>
2026-01-14 15:22:19 +0800

    init

---
+++ src/main/java/com/learn/projectGuide/ProjectGuideApplication.java
@@ -0,0 +1,13 @@
+package com.learn.projectGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGuideApplication.class, args);
+	}
+
+}
